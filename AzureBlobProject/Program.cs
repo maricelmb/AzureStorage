6fc13612a3077148d27dee300a38911b0625f39e
maricel <maricelmb@gmail.com>
2025-06-19 16:03:51 +1000

    Implement a container service

--- AzureBlobProject/Program.cs
+++ AzureBlobProject/Program.cs
@@ -1,10 +1,12 @@
 using Azure.Storage.Blobs;
+using AzureBlobProject.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton(u => new BlobServiceClient(builder.Configuration.GetValue<string>("BlobConnection")));
+builder.Services.AddSingleton<IContainerService, ContainerService>();
 
 var app = builder.Build();
 
---
+++ AzureBlobProject/Services/ContainerService.cs
@@ -0,0 +1,47 @@
+﻿
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public ContainerService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;        
+        }
+
+        public async Task CreateContainer(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+
+            await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer);
+        }
+
+        public async Task DeleteContainer(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+
+            await blobContainerClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<List<string>> GetAllContainer()
+        {
+            List<string> containerNames = new();
+
+            await foreach(BlobContainerItem item in _blobServiceClient.GetBlobContainersAsync())
+            {
+                containerNames.Add(item.Name);
+            }
+
+            return containerNames;
+        }
+
+        public Task<List<string>> GetAllContainersAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
