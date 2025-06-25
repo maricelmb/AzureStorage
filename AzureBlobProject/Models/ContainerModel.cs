3d88b139d29b912467dd556039022ba9668cd905
maricel <maricelmb@gmail.com>
2025-06-25 13:34:34 +1000

    Add create form & implement delete action

---
+++ AzureBlobProject/Models/ContainerModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace AzureBlobProject.Models
+{
+    public class ContainerModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
