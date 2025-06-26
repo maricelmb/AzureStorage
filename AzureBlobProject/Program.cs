44fc3acff0ae6736e30e5eac52d94255ca0929ad
maricel <maricelmb@gmail.com>
2025-06-26 23:14:34 +1000

    Add a blob service and model

--- AzureBlobProject/Program.cs
+++ AzureBlobProject/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton(u => new BlobServiceClient(builder.Configuration.GetValue<string>("BlobConnection")));
 builder.Services.AddSingleton<IContainerService, ContainerService>();
+builder.Services.AddSingleton<IBlobService, BlobService>();
 
 var app = builder.Build();
 
